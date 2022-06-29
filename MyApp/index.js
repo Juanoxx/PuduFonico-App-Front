ece29091e865e90009d49c01c72cf1e090ecfc6d
Juan Arredondo <juan.arredondo.c@usach.cl>
2022-06-29 04:23:06 -0400

    Cambios VoiceTest

--- MyApp/index.js
+++ MyApp/index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import StackNavigator from './Appli/componentes/StackNavigator';
 import {name as appName} from './app.json';
 import vistaVacia from './Appli/screens/vistaVacia';
+import voiceTest from './Appli/screens/voiceTest';
 
-AppRegistry.registerComponent(appName, () => StackNavigator);
+AppRegistry.registerComponent(appName, () => voiceTest);
